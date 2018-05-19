2cb3ee94c151351c0e7601eea79378e028787751
Mattia Panzeri <mattia.panzeri93@gmail.com>
2018-05-19 02:00:10 +0200

    feat(Android): Finalize `Android` implementation

--- android/src/main/java/com/guidolodetti/RNStripeModule.java
+++ android/src/main/java/com/guidolodetti/RNStripeModule.java
@@ -1,21 +1,21 @@
 
 package com.guidolodetti;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.stripe.android.CustomerSession;
@@ -23,23 +23,39 @@ import com.stripe.android.EphemeralKeyProvider;
 import com.stripe.android.EphemeralKeyUpdateListener;
 import com.stripe.android.PaymentConfiguration;
 import com.stripe.android.PaymentSession;
-import com.stripe.android.view.PaymentMethodsActivity;
+import com.stripe.android.PaymentSessionConfig;
+import com.stripe.android.PaymentSessionData;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Customer;
+import com.stripe.android.model.CustomerSource;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.facebook.react.bridge.ReadableType.Array;
-import static com.facebook.react.bridge.ReadableType.Map;
+import java.util.HashMap;
+import java.util.Map;
 
-public class RNStripeModule extends ReactContextBaseJavaModule {
+public class RNStripeModule extends ReactContextBaseJavaModule implements PaymentSession.PaymentSessionListener {
 
-    private final ReactApplicationContext reactContext;
     private EphemeralKeyUpdateListener keyUpdateListener;
+
+    private PaymentSession mPaymentSession;
+
+    private Promise initPaymentContextPromise;
+
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(activity, requestCode, resultCode, data);
+            mPaymentSession.handlePaymentData(requestCode, resultCode, data);
+        }
+    };
 
     public RNStripeModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
+
+        // Add the listener for `onActivityResult`
+        reactContext.addActivityEventListener(mActivityEventListener);
     }
 
     @Override
@@ -48,102 +64,134 @@ public class RNStripeModule extends ReactContextBaseJavaModule {
     }
 
 
-    private void sendEvent(ReactContext reactContext,
-                           String eventName,
-                           @Nullable WritableMap params) {
-        reactContext
+    private void sendEvent(String eventName, @Nullable WritableMap params) {
+        getReactApplicationContext()
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit(eventName, params);
     }
 
     @ReactMethod
-    public void initPaymentContext(ReadableMap options) {
-        PaymentConfiguration.init(options.getString("publishableKey"));
+    public void initWithOptions(ReadableMap options, Promise promise) {
+        final String publishableKey = options.getString("publishableKey");
+        if (publishableKey == null) {
+            promise.reject("RNStripePublishableKeyRequired", "A valid Stripe PublishableKey is required");
+            return;
+        }
+
+        PaymentConfiguration.init(publishableKey);
+
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void initPaymentContext(ReadableMap options, Promise promise) {
+        if (initPaymentContextPromise != null) {
+            promise.reject("RNStripeInitPaymentInProgress", "initPaymentContext already called, but initialization is not yet completed.");
+            return;
+        }
+
+        initPaymentContextPromise = promise;
+
+        // Init CustomerSession
         CustomerSession.initCustomerSession(new EphemeralKeyProvider() {
             @Override
             public void createEphemeralKey(@NonNull String apiVersion,
                                            @NonNull EphemeralKeyUpdateListener _keyUpdateListener) {
-                Log.e("RNStripe", "Init");
+                Log.d("RNStripe", "initPaymentContext");
                 keyUpdateListener = _keyUpdateListener;
                 WritableMap params = Arguments.createMap();
                 params.putString("apiVersion", apiVersion);
-                sendEvent(getReactApplicationContext(),
-                        "RNStripeRequestedCustomerKey", params);
+
+                sendEvent("RNStripeRequestedCustomerKey", params);
             }
         });
+
+        // Init PaymentSession
+        mPaymentSession = new PaymentSession(getReactApplicationContext().getCurrentActivity());
+        mPaymentSession.init(this, new PaymentSessionConfig.Builder().setShippingInfoRequired(true).build());
     }
 
     @ReactMethod
     public void retrievedCustomerKey(ReadableMap customerKey) {
         try {
-            JSONObject customerKeyJson = convertMapToJson(customerKey);
-            Log.e("RNStripe", "Stripe customer key: "+customerKeyJson.toString());
+            JSONObject customerKeyJson = RNStripeUtils.convertMapToJson(customerKey);
+            Log.d("RNStripe", "Stripe customer key: " + customerKeyJson.toString());
             if (keyUpdateListener != null) {
                 keyUpdateListener.onKeyUpdate(customerKeyJson.toString());
             }
         } catch (JSONException e) {
-            Log.e("RNStripe", "JSON Convertion error");
+            Log.e("RNStripe", "JSON Conversion error");
         }
     }
 
     @ReactMethod
+    public void failedRetrievingCustomerKey() {
+        Log.e("RNStripe", "Failed to retrieve CustomerKey");
+    }
+
+    @ReactMethod
     public void presentPaymentMethodsViewController(Promise promise) {
-        Intent payIntent = PaymentMethodsActivity.newIntent(getReactApplicationContext());
-        getReactApplicationContext().startActivity(payIntent);
+        mPaymentSession.presentPaymentMethodSelection();
+        promise.resolve(true);
     }
 
-    private static JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
-        JSONObject object = new JSONObject();
-        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
-        while (iterator.hasNextKey()) {
-            String key = iterator.nextKey();
-            switch (readableMap.getType(key)) {
-                case Null:
-                    object.put(key, JSONObject.NULL);
-                    break;
-                case Boolean:
-                    object.put(key, readableMap.getBoolean(key));
-                    break;
-                case Number:
-                    object.put(key, readableMap.getDouble(key));
-                    break;
-                case String:
-                    object.put(key, readableMap.getString(key));
-                    break;
-                case Map:
-                    object.put(key, convertMapToJson(readableMap.getMap(key)));
-                    break;
-                case Array:
-                    object.put(key, convertArrayToJson(readableMap.getArray(key)));
-                    break;
-            }
-        }
-        return object;
+    /**
+     * PaymentSessionListener
+     */
+
+    @Override
+    public void onCommunicatingStateChanged(boolean isCommunicating) {
+
     }
 
-    private static JSONArray convertArrayToJson(ReadableArray readableArray) throws JSONException {
-        JSONArray array = new JSONArray();
-        for (int i = 0; i < readableArray.size(); i++) {
-            switch (readableArray.getType(i)) {
-                case Null:
-                    break;
-                case Boolean:
-                    array.put(readableArray.getBoolean(i));
-                    break;
-                case Number:
-                    array.put(readableArray.getDouble(i));
-                    break;
-                case String:
-                    array.put(readableArray.getString(i));
-                    break;
-                case Map:
-                    array.put(convertMapToJson(readableArray.getMap(i)));
-                    break;
-                case Array:
-                    array.put(convertArrayToJson(readableArray.getArray(i)));
-                    break;
-            }
-        }
-        return array;
+    @Override
+    public void onError(int errorCode, @Nullable String errorMessage) {
+        Toast.makeText(getReactApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentSessionDataChanged(@NonNull PaymentSessionData data) {
+        final String selectedPaymentMethodId = data.getSelectedPaymentMethodId();
+        CustomerSession.getInstance().retrieveCurrentCustomer(
+                new CustomerSession.CustomerRetrievalListener() {
+                    @Override
+                    public void onCustomerRetrieved(@NonNull Customer customer) {
+                        final CustomerSource displaySource = customer.getSourceById(selectedPaymentMethodId);
+                        if (displaySource == null) {
+                            if (initPaymentContextPromise != null) {
+                                initPaymentContextPromise.resolve(null);
+                                initPaymentContextPromise = null;
+                            }
+                            return;
+                        }
+
+                        final Card customerCard = displaySource.asCard();
+                        if (customerCard == null) {
+                            if (initPaymentContextPromise != null) {
+                                initPaymentContextPromise.resolve(null);
+                                initPaymentContextPromise = null;
+                            }
+                            return;
+                        }
+
+                        final Map<String, Object> selectedCardDetails = new HashMap<>();
+                        selectedCardDetails.put("label", customerCard.getBrand() + " " + customerCard.getLast4());
+                        selectedCardDetails.put("cardTemplateImage", "");
+
+                        // Send the card information to JS
+                        RNStripeModule.this.sendEvent("RNStripeSelectedPaymentMethodDidChange", Arguments.makeNativeMap(selectedCardDetails));
+
+                        if (initPaymentContextPromise != null) {
+                            initPaymentContextPromise.resolve(Arguments.makeNativeMap(selectedCardDetails));
+                            initPaymentContextPromise = null;
+                        }
+                    }
+
+                    @Override
+                    public void onError(int errorCode, @Nullable String errorMessage) {
+                        Toast.makeText(getReactApplicationContext(), errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
